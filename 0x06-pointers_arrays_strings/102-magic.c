3a0e523ec60555f52f28eee55a112a920b15933a
M0941266845 <melakuagmas@gmail.com>
2023-03-05 09:51:35 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main -  allowed to use a and modify p the variabl
+ *
+ * Return: 0
+ */
 int main(void)
 {
 int n;
@@ -15,7 +20,7 @@ p = &n;
 * - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
-/* ...so that this prints 98\n */
+ /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
